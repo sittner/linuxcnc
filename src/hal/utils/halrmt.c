d08e5a8c893e01310b10f84da292be664a7d5731
Sascha Ittner <sascha.ittner@modusoft.de>
2026-03-21 12:57:39 +0100

    fixed compiler warnings

--- src/hal/utils/halrmt.c
+++ src/hal/utils/halrmt.c
@@ -1304,7 +1304,6 @@ static int doUnload(char *mod_name, connectionRecType *context)
 static int unloadrt_comp(char *mod_name)
 {
     int retval, status;
-    char *argv[4];
     pid_t pid;
 
     /* now we need to fork, and then exec rmmod.... */
