b9b7a7432d94d57c03f79b0eca9d650cf6dc066d
Sascha Ittner <sascha.ittner@modusoft.de>
2026-04-27 09:58:18 +0200

    fixed indent

--- src/emc/usr_intf/axis/scripts/axis.py
+++ src/emc/usr_intf/axis/scripts/axis.py
@@ -1026,16 +1026,16 @@ class LivePlotter:
         vupdate(vars.on_any_limit, on_any_limit)
         global current_tool
         current_tool = self.stat.tool_table[0]
-        if current_tool:
-            tool_data = {'tool': current_tool[0], 'zo': current_tool[3], 'xo': current_tool[1], 'dia': current_tool[10]}
         if current_tool is None:
             vupdate(vars.tool, _("Unknown tool %d") % self.stat.tool_in_spindle)
-        elif tool_data['tool'] == 0 or tool_data['tool'] == -1:
-            vupdate(vars.tool, _("No tool"))
-        elif current_tool.xoffset == 0 and not lathe:
-            vupdate(vars.tool, _("Tool %(tool)d, offset %(zo)g, diameter %(dia)g") % tool_data)
         else:
-            vupdate(vars.tool, _("Tool %(tool)d, zo %(zo)g, xo %(xo)g, dia %(dia)g") % tool_data)
+            tool_data = {'tool': current_tool[0], 'zo': current_tool[3], 'xo': current_tool[1], 'dia': current_tool[10]}
+            if tool_data['tool'] == 0 or tool_data['tool'] == -1:
+                vupdate(vars.tool, _("No tool"))
+            elif current_tool.xoffset == 0 and not lathe:
+                vupdate(vars.tool, _("Tool %(tool)d, offset %(zo)g, diameter %(dia)g") % tool_data)
+            else:
+                vupdate(vars.tool, _("Tool %(tool)d, zo %(zo)g, xo %(xo)g, dia %(dia)g") % tool_data)
         active_codes = []
         for i in self.stat.gcodes[1:]:
             if i == -1: continue
