93d292b3bf364e1db8a9cf17366de6d4b1ab1a6b
Sascha Ittner <sascha.ittner@modusoft.de>
2026-05-19 00:12:05 +0200

    cleanup old gcodemodule stuff

--- lib/python/rs274/glcanon.py
+++ lib/python/rs274/glcanon.py
@@ -24,7 +24,7 @@ import _glhelpers
 import gmi
 from gmi.constants import KINEMATICS_IDENTITY, MAX_AXIS
 import array
-import gcode_rest as gcode
+import gcode
 import os
 import re
 from functools import reduce
--- lib/python/rs274/interpret.py
+++ lib/python/rs274/interpret.py
@@ -15,7 +15,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 import math
-import gcode_rest as gcode
+import gcode
 
 class Translated:
     g92_offset_x = g92_offset_y = g92_offset_z = 0
